59c8abf068613c01e13248e8addecdcef162a9c5
NeonCardinal <nvnnaidenov@gmail.com>
2022-11-17 00:22:12 +0200

    add

---
+++ 001.FirstStepsInCodingLab/008.PetShop/PetShop.cs
@@ -0,0 +1,19 @@
+﻿//Chapter First steps in programmin - lab, Task 008
+//17.11.2022, 00:09
+using System;
+
+public class PetShop
+{
+    static void Main()
+    {
+        //Read input
+        int dogFoodPackets = int.Parse(Console.ReadLine());
+        int catFoodPackets = int.Parse(Console.ReadLine());
+
+        //Calculations
+        double total = dogFoodPackets * 2.50 + catFoodPackets * 4;
+
+        //Print result on the console
+        Console.WriteLine($"{total} lv.");
+    }
+}
