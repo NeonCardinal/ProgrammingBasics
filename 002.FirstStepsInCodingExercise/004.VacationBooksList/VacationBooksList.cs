7cd821bd96c0601a7dd09ab9c74617fe9c9960e8
NeonCardinal <nvnnaidenov@gmail.com>
2022-11-18 00:15:20 +0200

    add

---
+++ 002.FirstStepsInCodingExercise/004.VacationBooksList/VacationBooksList.cs
@@ -0,0 +1,21 @@
+﻿//Chapter First steps in programming - exercise, Task 004
+//17.11.2022, 23:36
+using System;
+
+public class VacationBooksList
+{
+    static void Main()
+    {
+        //Read input
+        int pagesCounter = int.Parse(Console.ReadLine());
+        int pagesPerHour = int.Parse(Console.ReadLine());
+        int days = int.Parse(Console.ReadLine());
+
+        //Calculations
+        int totalTimeForBook = pagesCounter / pagesPerHour;
+        int neededHours = totalTimeForBook / days;
+
+        //Print result on the console
+        Console.WriteLine(neededHours);
+    }
+}
