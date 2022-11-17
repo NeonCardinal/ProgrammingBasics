c3f9e2636f5a6789ce8b10be75387caa0d0fdaea
NeonCardinal <nvnnaidenov@gmail.com>
2022-11-18 00:14:42 +0200

    add

---
+++ 002.FirstStepsInCodingExercise/002.RadiansToDegrees/RadiansToDegrees.cs
@@ -0,0 +1,18 @@
+﻿//Chapter First steps in coding - exercise
+//17.11.2022, 23:27
+using System;
+
+public class RadiansToDegrees
+{
+    static void Main()
+    {
+        //Read input
+        double radians = double.Parse(Console.ReadLine());
+
+        //Calculations
+        double degrees = radians * 180 / Math.PI;
+
+        //Print result on the console
+        Console.WriteLine(degrees);
+    }
+}
