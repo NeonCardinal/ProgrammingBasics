a0c27f4716583bf98817215a870862e57c9e42d0
NeonCardinal <nvnnaidenov@gmail.com>
2022-11-18 00:16:21 +0200

    add

---
+++ 002.FirstStepsInCodingExercise/008.BasketballEquipment/BasketballEquipment.cs
@@ -0,0 +1,22 @@
+﻿//Chapter First steps in programmin - exercise, Task 008
+//18.11.2022, 00:00
+using System;
+
+public class BasketballEquipment
+{
+    static void Main()
+    {
+        //Read input
+        int pricePerYear = int.Parse(Console.ReadLine());
+
+        //Calculations
+        double priceForShoes = pricePerYear * 0.60;
+        double pricePerEquipe = priceForShoes * 0.80;
+        double priceForBall = pricePerEquipe / 4;
+        double priceForAcc = priceForBall / 5;
+        double total = pricePerYear + priceForShoes + pricePerEquipe + priceForBall + priceForAcc;
+
+        //Print result on the console
+        Console.WriteLine(total);
+    }
+}
