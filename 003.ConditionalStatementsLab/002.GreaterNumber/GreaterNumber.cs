b22a5f126fa7a407cd7720c3a41735711f020386
NeonCardinal <nvnnaidenov@gmail.com>
2022-11-21 01:34:11 +0200

    add

---
+++ 003.ConditionalStatementsLab/002.GreaterNumber/GreaterNumber.cs
@@ -0,0 +1,23 @@
+﻿//Chapter Conditional statements - lab, Task 002
+//18.11.2022, 01:19
+using System;
+
+public class GreaterNumber
+{
+    static void Main()
+    {
+        //Read input
+        int a = int.Parse(Console.ReadLine());
+        int b = int.Parse(Console.ReadLine());
+
+        //Logic and result
+        if(a >= b)
+        {
+            Console.WriteLine(a);
+        }
+        else
+        {
+            Console.WriteLine(b);
+        }
+    }
+}
