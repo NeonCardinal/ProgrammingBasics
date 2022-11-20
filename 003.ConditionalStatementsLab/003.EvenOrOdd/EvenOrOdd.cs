847ddbfa1b18881edce517094092491c0fbeb5ad
NeonCardinal <nvnnaidenov@gmail.com>
2022-11-21 01:34:28 +0200

    add

---
+++ 003.ConditionalStatementsLab/003.EvenOrOdd/EvenOrOdd.cs
@@ -0,0 +1,22 @@
+﻿//Chapter Conditional statements - lab, Task 003
+//18.11.2022, 01:23
+using System;
+
+public class EvenOrOdd
+{
+    static void Main()
+    {
+        //Read input
+        int number = int.Parse(Console.ReadLine());
+
+        //Logic and result
+        if(number % 2 == 0)
+        {
+            Console.WriteLine("even");
+        }
+        else
+        {
+            Console.WriteLine("odd");
+        }
+    }
+}
