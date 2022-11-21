3ca5b08bd5fb7bc3d5a4ad22eb5c09d719a40f25
NeonCardinal <nvnnaidenov@gmail.com>
2022-11-22 00:04:05 +0200

    add

---
+++ 004.ConditionalStatementsExercise/003.TimePlusFifteenMinutes/TimePlusFifteenMinutes.cs
@@ -0,0 +1,35 @@
+﻿//Chapter Conditional statements - exercise, Task 003
+//19.11.2022, 00:42
+using System;
+
+public class TimePlusFifteenMinutes
+{
+    static void Main()
+    {
+        //Read input
+        int hours = int.Parse(Console.ReadLine());
+        int minutes = int.Parse(Console.ReadLine());
+
+        //Logic and calculations
+        minutes += 15;
+
+        if (minutes > 59)
+        {
+            hours++;
+            minutes -= 60;
+        }
+        if (hours > 23)
+        {
+            hours -= 24;
+        }
+
+        if (minutes < 10)
+        {
+            Console.WriteLine($"{hours}:0{minutes}");
+        }
+        else
+        {
+            Console.WriteLine($"{hours}:{minutes}");
+        }
+    }
+}
