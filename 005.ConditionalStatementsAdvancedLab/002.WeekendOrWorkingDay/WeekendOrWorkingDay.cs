2b6cd3d540fe0c82b6e3193b0c7ed34d5d5ee797
NeonCardinal <nvnnaidenov@gmail.com>
2022-11-24 20:04:54 +0200

    add

---
+++ 005.ConditionalStatementsAdvancedLab/002.WeekendOrWorkingDay/WeekendOrWorkingDay.cs
@@ -0,0 +1,29 @@
+﻿//Chapter Conditional statements advanced - lab, Task 002;
+//23.11.2022, 00:14
+using System;
+
+public class WeekendOrWorkingDay
+{
+    static void Main()
+    {
+        string day = Console.ReadLine();
+
+        switch(day)
+        {
+            case "Monday":
+            case "Tuesday":
+            case "Wednesday":
+            case "Thursday":
+            case "Friday":
+                Console.WriteLine("Working day");
+                break;
+            case "Saturday":
+            case "Sunday":
+                Console.WriteLine("Weekend");
+                break;
+            default:
+                Console.WriteLine("Error");
+                break;
+        }
+    }
+}
