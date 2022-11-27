f1cdf6ebe4dd7c592f086bc08b41d95fffd28449
NeonCardinal <nvnnaidenov@gmail.com>
2022-11-27 12:02:19 +0200

    add

---
+++ 007.ForLoopLab/004.EvenPowersOfTwo/EvenPowersOfTwo.cs
@@ -0,0 +1,16 @@
+﻿//Chapter For loop - lab, Task 004
+//26.11.2022, 21:21
+using System;
+
+public class EvenPowersOfTwo
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        for(int i = 0; i <= n; i += 2)
+        {
+            Console.WriteLine(Math.Pow(2, i));
+        }
+    }
+}
