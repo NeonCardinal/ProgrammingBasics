f42a014e153a2a9d37fd975fee155165a38db3af
NeonCardinal <nvnnaidenov@gmail.com>
2022-11-27 11:56:47 +0200

    add

---
+++ 007.ForLoopLab/002.NumbersFromNToOne/NumbersFromNToOne.cs
@@ -0,0 +1,16 @@
+﻿//Chapter For loop - lab, Task 002
+//26.11.2022, 21:16
+using System;
+
+public class NumbersFromNToOne
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        for(int i = n; i >= 1; i--)
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
