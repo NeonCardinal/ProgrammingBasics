970b26ee93eaf39a21fb2f21dec39e7a12dcb8a0
NeonCardinal <nvnnaidenov@gmail.com>
2022-11-27 11:56:22 +0200

    add

---
+++ 007.ForLoopLab/001.NumbersFromOneToHundred/NumbersFromOneToHundred.cs
@@ -0,0 +1,14 @@
+﻿//Chapter For loop - lab, Task 001
+//26.11.2022, 21:13
+using System;
+
+public class NumbersFromOneToHundred
+{
+    static void Main()
+    {
+        for(int i = 1; i <= 100; i++)
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
