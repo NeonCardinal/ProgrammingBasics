ea59dfa68e41c831bd0d5650f295b76c5131f0d4
NeonCardinal <nvnnaidenov@gmail.com>
2022-11-27 12:02:41 +0200

    add

---
+++ 007.ForLoopLab/005.CharacterSequence/CharacterSequence.cs
@@ -0,0 +1,18 @@
+﻿//Chapter For loop - lab, Task 005
+//26.11.2022, 21:24
+using System;
+
+public class CharacterSequence
+{
+    static void Main()
+    {
+        string text = Console.ReadLine();
+
+        for(int i = 0; i < text.Length; i++)
+        {
+            char c = text[i];
+
+            Console.WriteLine(c);
+        }
+    }
+}
