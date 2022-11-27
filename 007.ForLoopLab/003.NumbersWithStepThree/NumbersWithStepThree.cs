44da50a46129244d4e0c4e8297443cce518e26af
NeonCardinal <nvnnaidenov@gmail.com>
2022-11-27 11:57:08 +0200

    add

---
+++ 007.ForLoopLab/003.NumbersWithStepThree/NumbersWithStepThree.cs
@@ -0,0 +1,16 @@
+﻿//Chapter For loop - lab, Task 003
+//26.11.2022, 21:18
+using System;
+
+public class NumbersWithStepThree
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        for(int i = 1; i <= n; i += 3)
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
