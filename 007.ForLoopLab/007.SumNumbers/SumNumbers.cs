6015c67f2a56758577783cffdacb2f2d3a201cb8
NeonCardinal <nvnnaidenov@gmail.com>
2022-11-27 12:03:25 +0200

    add

---
+++ 007.ForLoopLab/007.SumNumbers/SumNumbers.cs
@@ -0,0 +1,22 @@
+﻿//Chapter For loop - lab, Task 007
+//26.11.2022, 21:32
+using System;
+
+public class SumNumbers
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        int sum = 0;
+
+        for(int i = 0; i < n; i++)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            sum += number;
+        }
+
+        Console.WriteLine(sum);
+    }
+}
