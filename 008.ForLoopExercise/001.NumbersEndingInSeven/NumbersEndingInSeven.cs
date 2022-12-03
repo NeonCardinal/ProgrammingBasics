11bac939db7da2b5a7946ef069cc79dba7c65877
NeonCardinal <nvnnaidenov@gmail.com>
2022-12-03 23:37:27 +0200

    add

---
+++ 008.ForLoopExercise/001.NumbersEndingInSeven/NumbersEndingInSeven.cs
@@ -0,0 +1,17 @@
+﻿//Chapter For loop - exercise, Task 001
+//28.11.2022, 21:57
+using System;
+
+public class NumbersEndingInSeven
+{
+    static void Main()
+    {
+        for(int i = 1; i <= 1000; i++)
+        {
+            if(i % 10 == 7)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
