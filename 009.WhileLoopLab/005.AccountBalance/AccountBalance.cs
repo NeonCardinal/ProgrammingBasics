6e87641228197b12ad3cf2db48653c5410b1d56d
NeonCardinal <nvnnaidenov@gmail.com>
2022-12-06 00:31:13 +0200

    add

---
+++ 009.WhileLoopLab/005.AccountBalance/AccountBalance.cs
@@ -0,0 +1,33 @@
+﻿//Chapter While loop - lab, Task 005
+//05.12.2022, 23:45
+using System;
+
+public class AccountBalance
+{
+    static void Main()
+    {
+        string command = Console.ReadLine();
+        
+        double total = 0.0;
+
+        while (command != "NoMoreMoney")
+        {
+            double add = double.Parse(command);
+
+            if(add <= 0)
+            {
+                Console.WriteLine("Invalid operation!");
+                break;
+            }
+            else
+            {
+                Console.WriteLine($"Increase: {add:F2}");
+                total += add;
+            }
+
+            command = Console.ReadLine();
+        }
+
+        Console.WriteLine($"Total: {total:F2}");
+    }
+}
