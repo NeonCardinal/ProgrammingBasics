74698277a9eb3a31f0dbf45fc793c73f316d65bd
NeonCardinal <nvnnaidenov@gmail.com>
2022-12-06 00:30:54 +0200

    add

---
+++ 009.WhileLoopLab/004.Sequence/Sequence.cs
@@ -0,0 +1,20 @@
+﻿//Chapter While loop - lab, Task 004
+//05.12.2022, 23:40
+using System;
+
+public class Sequence
+{
+    static void Main()
+    {
+        int number = int.Parse(Console.ReadLine());
+
+        int k = 1;
+
+        while(k <= number)
+        {
+            Console.WriteLine(k);
+
+            k = k * 2 + 1;
+        }
+    }
+}
