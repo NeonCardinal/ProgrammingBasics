e9f4c62a2bed3398fa540be1789a14a3602a50b3
NeonCardinal <nvnnaidenov@gmail.com>
2022-12-06 00:30:22 +0200

    add

---
+++ 009.WhileLoopLab/002.Password/Password.cs
@@ -0,0 +1,21 @@
+﻿//Chapter While loop - lab, Task 002
+//05.12.2022, 23:30
+using System;
+
+public class Password
+{
+    static void Main()
+    {
+        string user = Console.ReadLine();
+        string password = Console.ReadLine();
+
+        string input = Console.ReadLine();
+
+        while(input != password)
+        {
+            input = Console.ReadLine();
+        }
+
+        Console.WriteLine($"Welcome {user}!");
+    }
+}
