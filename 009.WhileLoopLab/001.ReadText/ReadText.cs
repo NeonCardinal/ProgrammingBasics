36f7be25d9247a181b9d22812918f1b86acc5094
NeonCardinal <nvnnaidenov@gmail.com>
2022-12-06 00:30:07 +0200

    add

---
+++ 009.WhileLoopLab/001.ReadText/ReadText.cs
@@ -0,0 +1,18 @@
+﻿//Chapter While loop - lab, Task 001
+//05.12.2022, 23:28
+using System;
+
+public class ReadText
+{
+    static void Main()
+    {
+        string text = Console.ReadLine();
+
+        while(text != "Stop")
+        {
+            Console.WriteLine(text);
+
+            text = Console.ReadLine();
+        }
+    }
+}
