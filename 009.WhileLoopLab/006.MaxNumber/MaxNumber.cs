05a8ed6468f00ac876e1a1c3d45debdbe36ec4eb
NeonCardinal <nvnnaidenov@gmail.com>
2022-12-06 00:31:30 +0200

    add

---
+++ 009.WhileLoopLab/006.MaxNumber/MaxNumber.cs
@@ -0,0 +1,27 @@
+﻿//Chapter While loop - lab, Task 006
+//06.12.2022, 00:02
+using System;
+
+public class MaxNumber
+{
+    static void Main()
+    {
+        string command = Console.ReadLine();
+
+        int max = int.MinValue;
+
+        while(command != "Stop")
+        {
+            int number = int.Parse(command);
+
+            if(number >= max)
+            {
+                max = number;
+            }
+
+            command = Console.ReadLine();
+        }
+
+        Console.WriteLine(max);
+    }
+}
