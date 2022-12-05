9d7d892c1975e12f9cd6dc746a0cc71bf377f067
NeonCardinal <nvnnaidenov@gmail.com>
2022-12-06 00:30:38 +0200

    add

---
+++ 009.WhileLoopLab/003.SumNumbers/SumNumbers.cs
@@ -0,0 +1,20 @@
+﻿//Chapter While loop - lab, Task 003
+//05.12.2022, 23:35
+using System;
+
+public class SumNumbers
+{
+    static void Main()
+    {
+        int number = int.Parse(Console.ReadLine());
+
+        int sum = 0;
+
+        while(sum < number)
+        {
+            sum += int.Parse(Console.ReadLine());
+        }
+
+        Console.WriteLine(sum);
+    }
+}
