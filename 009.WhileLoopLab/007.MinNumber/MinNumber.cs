e9c627e369f5b57dd3ee8bd52e6c0c2140c6c346
NeonCardinal <nvnnaidenov@gmail.com>
2022-12-06 00:31:45 +0200

    add

---
+++ 009.WhileLoopLab/007.MinNumber/MinNumber.cs
@@ -0,0 +1,27 @@
+﻿//Chapter While loop - lab, Task 007
+//06.12.2022, 00:06
+using System;
+
+public class MinNumber
+{
+    static void Main()
+    {
+        string command = Console.ReadLine();
+
+        int min = int.MaxValue;
+
+        while(command != "Stop")
+        {
+            int number = int.Parse(command);
+
+            if(number <= min)
+            {
+                min = number;
+            }
+
+            command = Console.ReadLine();
+        }
+
+        Console.WriteLine(min);
+    }
+}
