10f7ce7a77fd9fe6f6eddba4a4ad67e1d5c957bd
NeonCardinal <nvnnaidenov@gmail.com>
2022-12-05 23:03:59 +0200

    add

---
+++ 008.ForLoopExercise/006.Oscars/Oscars.cs
@@ -0,0 +1,35 @@
+﻿//Chapter For loop - exercise, Task 006
+//05.12.2022, Task 006
+using System;
+
+public class Oscars
+{
+    static void Main()
+    {
+        string actior = Console.ReadLine();
+        double score = double.Parse(Console.ReadLine());
+        int n = int.Parse(Console.ReadLine());
+
+        double addedPoints = 0.0;
+
+        for (int i = 0; i < n; i++)
+        {
+            string name = Console.ReadLine();
+            double points = double.Parse(Console.ReadLine());
+
+            addedPoints = name.Length * (points / 2);
+            score += addedPoints;
+
+            if (score > 1250.5)
+            {
+                Console.WriteLine($"Congratulations, {actior} got a nominee for leading role with {score:F1}!");
+                break;
+            }
+        }
+
+        if (score < 1250.5)
+        {
+            Console.WriteLine($"Sorry, {actior} you need {(1250.5 - score):F1} more!");
+        }
+    }
+}
