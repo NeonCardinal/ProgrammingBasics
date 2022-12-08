0accee28898f99f4357d0293a7678f21e44be932
NeonCardinal <nvnnaidenov@gmail.com>
2022-12-08 20:44:21 +0200

    add

---
+++ 010.WhileLoopExercise/001.OldBooks/OldBooks.cs
@@ -0,0 +1,31 @@
+﻿//Chapter While loop - exercise, Task 001
+//08.12.2022, 19:30
+using System;
+
+public class OldBooks
+{
+    static void Main()
+    {
+        int bookCounter = 0;
+        string bookName = Console.ReadLine();
+
+        while(true)
+        {
+            string text = Console.ReadLine();
+
+            if(text == "No More Books")
+            {
+                Console.WriteLine("The book you search is not here!");
+                Console.WriteLine($"You checked {bookCounter} books.");
+                break;
+            }
+
+            if(text == bookName)
+            {
+                Console.WriteLine($"You checked {bookCounter} books and found it.");
+                break;
+            }
+            bookCounter++;
+        }
+    }
+}
