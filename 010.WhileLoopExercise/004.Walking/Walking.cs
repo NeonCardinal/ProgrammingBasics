7b307067b21a79acdf5bb72de6f2e99f669c4fa0
NeonCardinal <nvnnaidenov@gmail.com>
2022-12-08 20:45:14 +0200

    add

---
+++ 010.WhileLoopExercise/004.Walking/Walking.cs
@@ -0,0 +1,42 @@
+﻿//Chapter While loop - exercise, Task 004
+//08.12.2022
+using System;
+
+public class Walking
+{
+    static void Main()
+    {
+        int steps = 0;
+
+        while (true)
+        {
+            string command = Console.ReadLine();
+
+            if (command == "Going home")
+            {
+                int toHome = int.Parse(Console.ReadLine());
+                steps += toHome;
+                if (steps >= 10000)
+                {
+                    Console.WriteLine("Goal reached! Good job!");
+                    Console.WriteLine($"{steps - 10000} steps over the goal!");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine($"{10000 - steps} more steps to reach goal.");
+                }
+                break;
+            }
+
+            steps += int.Parse(command);
+
+            if (steps >= 10000)
+            {
+                Console.WriteLine("Goal reached! Good job!");
+                Console.WriteLine($"{steps - 10000} steps over the goal!");
+                break;
+            }
+        }
+    }
+}
