9d30fbdb96aaca73f163a170a6ff190db9b339c7
NeonCardinal <nvnnaidenov@gmail.com>
2022-12-16 00:04:56 +0200

    add

---
+++ 013.PBOnlineExamMarchOne/007.GameNumberWars/GameNumberWars.cs
@@ -0,0 +1,64 @@
+﻿//Chapter PB exam March 2019, Task 007
+//15.12.2022, 18:05
+using System;
+
+public class GameNumberWars
+{
+    static void Main()
+    {
+        string firstPlayer = Console.ReadLine();
+        string secondPlayer = Console.ReadLine();
+
+        int firstPlayerScore = 0;
+        int secondPlayerScore = 0;
+
+        string command = string.Empty;
+
+        while(true)
+        {
+            command = Console.ReadLine();
+
+            if(command == "End of game")
+            {
+                Console.WriteLine($"{firstPlayer} has {firstPlayerScore} points");
+                Console.WriteLine($"{secondPlayer} has {secondPlayerScore} points");
+                break;
+            }
+
+            int firstPlayerCard = int.Parse(command);
+
+            command = Console.ReadLine();
+
+            int secondPlayerCard = int.Parse(command);
+
+            if(firstPlayerCard > secondPlayerCard)
+            {
+                firstPlayerScore += firstPlayerCard - secondPlayerCard;
+            }
+            else if(secondPlayerCard > firstPlayerCard)
+            {
+                secondPlayerScore += secondPlayerCard - firstPlayerCard;
+            }
+            else if(firstPlayerCard == secondPlayerCard)
+            {
+                Console.WriteLine("Number wars!");
+
+                command = Console.ReadLine();
+                firstPlayerCard = int.Parse(command);
+                command = Console.ReadLine();
+                secondPlayerCard = int.Parse(command);
+
+                if(firstPlayerCard > secondPlayerCard)
+                {
+                    Console.WriteLine($"{firstPlayer} is winner with {firstPlayerScore} points");
+                }
+                else if(secondPlayerCard > firstPlayerCard)
+                {
+                    Console.WriteLine($"{secondPlayer} is winner with {secondPlayerScore} points");
+                }
+
+                break;
+            }
+        }
+    }
+}
