1b8ac3210c1c89111658c2201d54d07d12a908b2
NeonCardinal <nvnnaidenov@gmail.com>
2022-12-16 00:03:38 +0200

    add

---
+++ 013.PBOnlineExamMarchOne/002.BasketballEquipment/BasketballEquipment.cs
@@ -0,0 +1,20 @@
+﻿//PB Exam March 2019, Task 002
+//11.12.2002, 15:03
+using System;
+
+public class BasketballEquipment
+{
+    static void Main()
+    {
+        double yearTax = double.Parse(Console.ReadLine());
+
+        double shoesPrice = yearTax * 0.60;
+        double equipPrice = shoesPrice * 0.80;
+        double ballPrice = equipPrice / 4;
+        double accPrice = ballPrice / 5;
+
+        double total = shoesPrice + equipPrice + ballPrice + accPrice + yearTax;
+
+        Console.WriteLine($"{total:F2}");
+    }
+}
