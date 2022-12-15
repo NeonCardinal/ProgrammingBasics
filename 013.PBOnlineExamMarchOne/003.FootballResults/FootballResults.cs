1da2aabe169f4fb24b077268b0b3523f9b475c9e
NeonCardinal <nvnnaidenov@gmail.com>
2022-12-16 00:04:07 +0200

    add

---
+++ 013.PBOnlineExamMarchOne/003.FootballResults/FootballResults.cs
@@ -0,0 +1,74 @@
+﻿//PB Exam March 2019, Task 003
+//11.12.2022, 15:43
+using System;
+using System.Linq;
+
+public class FootballResults
+{
+    static void Main(string[] args)
+    {
+        string firstGameResult = Console.ReadLine();
+        string secondGameResult = Console.ReadLine();
+        string thirdGameResult = Console.ReadLine();
+
+
+
+        char firstLetterFromFirstWord = firstGameResult[0];
+        char secondLetterFromFirstWord = firstGameResult[2];
+        char firstLetterFromSecondWord = secondGameResult[0];
+        char secondLetterFromSecondWord = secondGameResult[2];
+        char firstLetterFromThirdWord = thirdGameResult[0];
+        char secondLetterFromThirdWord = thirdGameResult[2];
+
+        int win = 0;
+        int lose = 0;
+        int draw = 0;
+
+        if (firstLetterFromFirstWord > secondLetterFromFirstWord)
+        {
+            win++;
+
+        }
+        else if ((firstLetterFromFirstWord < secondLetterFromFirstWord))
+        {
+            lose++;
+        }
+        else
+        {
+            draw++;
+        }
+
+        if (firstLetterFromSecondWord > secondLetterFromSecondWord)
+        {
+            win++;
+
+        }
+        else if (firstLetterFromSecondWord < secondLetterFromSecondWord)
+        {
+            lose++;
+        }
+        else
+        {
+            draw++;
+        }
+
+        if (firstLetterFromThirdWord > secondLetterFromThirdWord)
+        {
+            win++;
+
+        }
+        else if (firstLetterFromThirdWord < secondLetterFromThirdWord)
+        {
+            lose++;
+        }
+        else
+        {
+            draw++;
+        }
+
+        Console.WriteLine($"Team won {win} games.");
+        Console.WriteLine($"Team lost {lose} games.");
+        Console.WriteLine($"Drawn games: {draw}");
+
+    }
+}
