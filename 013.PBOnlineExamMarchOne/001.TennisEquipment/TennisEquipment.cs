2ba4f2b2272e49f7754db659bea742a4c3ebf77c
NeonCardinal <nvnnaidenov@gmail.com>
2022-12-16 00:03:22 +0200

    add

---
+++ 013.PBOnlineExamMarchOne/001.TennisEquipment/TennisEquipment.cs
@@ -0,0 +1,25 @@
+﻿//PB Exam March 2019, Task 001
+//11.12.2022, 12:49
+using System;
+
+public class TennisEquipment
+{
+    static void Main()
+    {
+        double tennisRaquetPrice = double.Parse(Console.ReadLine());
+        int tennisRaquetCnt = int.Parse(Console.ReadLine());
+        int shoesCnt = int.Parse(Console.ReadLine());
+
+        double shoesPrice = tennisRaquetPrice / 6;
+        double totalPriceForRaquet = tennisRaquetPrice * tennisRaquetCnt;
+        double totalPriceForShoes = shoesPrice * shoesCnt;
+        double thingsPrice = (totalPriceForRaquet + totalPriceForShoes) * 0.20;
+        double total = thingsPrice + totalPriceForRaquet + totalPriceForShoes;
+
+        double jocovic = total / 8;
+        double sponsors = total * 7 / 8;
+
+        Console.WriteLine($"Price to be paid by Djokovic {Math.Floor(jocovic)}");
+        Console.WriteLine($"Price to be paid by sponsors {Math.Ceiling(sponsors)}");
+    }
+}
