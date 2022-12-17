0ab17b03cea625cee122d569009247b47c06f395
NeonCardinal <nvnnaidenov@gmail.com>
2022-12-17 10:27:43 +0200

    add

---
+++ 014.PBOnlineExamAprilOne/007.EasterEggsBattle/EasterEggsBattle.cs
@@ -0,0 +1,43 @@
+﻿//Chapter PB exam April 2019, Task 007
+//15.12.2022, 21:41
+using System;
+
+public class EasterEggsBattle
+{
+    static void Main()
+    {
+        int firstPlayerEggs = int.Parse(Console.ReadLine());
+        int secondPlayerEggs = int.Parse(Console.ReadLine());
+
+        while(true)
+        {
+            string command = Console.ReadLine();
+
+            if(command == "End")
+            {
+                Console.WriteLine($"Player one has {firstPlayerEggs} eggs left.");
+                Console.WriteLine($"Player two has {secondPlayerEggs} eggs left.");
+                break;
+            }
+            if (command == "one")
+            {
+                secondPlayerEggs--;
+            }
+            else if(command == "two")
+            {
+                firstPlayerEggs--;
+            }
+
+            if(firstPlayerEggs == 0)
+            {
+                Console.WriteLine($"Player one is out of eggs. Player two has {secondPlayerEggs} eggs left.");
+                break;
+            }
+            else if(secondPlayerEggs == 0)
+            {
+                Console.WriteLine($"Player two is out of eggs. Player one has {firstPlayerEggs} eggs left.");
+                break;
+            }
+        }
+    }
+}
