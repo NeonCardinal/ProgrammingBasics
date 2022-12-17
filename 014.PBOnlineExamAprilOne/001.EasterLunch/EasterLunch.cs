3819b0f5335090be94f758fc654b875c01f866e1
NeonCardinal <nvnnaidenov@gmail.com>
2022-12-17 10:26:08 +0200

    add

---
+++ 014.PBOnlineExamAprilOne/001.EasterLunch/EasterLunch.cs
@@ -0,0 +1,19 @@
+﻿//Chapter PB exam April 2019, Task 001
+//15.12.2022, 20:24
+using System;
+
+public class EasterLunch
+{
+    static void Main()
+    {
+        int kozunakCnt = int.Parse(Console.ReadLine());
+        int eggsCnt = int.Parse(Console.ReadLine());
+        int cakesCnt = int.Parse(Console.ReadLine());
+
+        double price = kozunakCnt * 3.20 + eggsCnt * 4.35 + cakesCnt * 5.40;
+        double dyePrice = eggsCnt * 12 * 0.15;
+        double total = price + dyePrice;
+
+        Console.WriteLine($"{total:F2}");
+    }
+}
