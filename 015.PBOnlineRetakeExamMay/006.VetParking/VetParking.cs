a93cd13873ab24061f28626e2d22f3002e7a3b25
NeonCardinal <nvnnaidenov@gmail.com>
2022-12-18 15:10:45 +0200

    add

---
+++ 015.PBOnlineRetakeExamMay/006.VetParking/VetParking.cs
@@ -0,0 +1,20 @@
+﻿//Chapter PB online retake exam May 2019, Task 006
+//16.12.2022, 11:17
+using System;
+
+public class VetParking
+{
+    static void Main()
+    {
+        int days = int.Parse(Console.ReadLine());
+        int hours = int.Parse(Console.ReadLine());
+
+        for(int i = 1; i <= days; i++)
+        {
+            for(int j = 1; j <= hours; j++)
+            {
+
+            }
+        }
+    }
+}
