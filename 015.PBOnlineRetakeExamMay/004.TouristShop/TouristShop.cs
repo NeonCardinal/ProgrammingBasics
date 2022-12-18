c44e076542c7a52c2890a7b0edbeaf49a5e26078
NeonCardinal <nvnnaidenov@gmail.com>
2022-12-18 15:10:03 +0200

    add

---
+++ 015.PBOnlineRetakeExamMay/004.TouristShop/TouristShop.cs
@@ -0,0 +1,45 @@
+﻿//Chapter PB online retake exam May 2019, Task 004
+//16.12.2022, 01:17
+using System;
+
+public class TouristShop
+{
+    static void Main()
+    {
+        double budjet = double.Parse(Console.ReadLine());
+
+        double productCnt = 1;
+        double sum = 0.00;
+
+        while (true)
+        {
+            string product = Console.ReadLine();
+
+            if (product == "Stop")
+            {
+                Console.WriteLine($"You bought {productCnt - 1} products for {sum:F2} leva.");
+                break;
+            }
+
+            double price = double.Parse(Console.ReadLine());
+
+            if(productCnt % 3 == 0)
+            {
+                price = price * 0.50;
+            }
+
+            if (price > budjet)
+            {
+                Console.WriteLine("You don't have enough money!");
+                Console.WriteLine($"You need {(price - budjet):F2} leva!");
+                break;
+            }
+            else
+            {
+                productCnt++;
+                sum += price;
+                budjet -= price;
+            }
+        }
+    }
+}
