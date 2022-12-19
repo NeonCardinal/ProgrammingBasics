012a9a96c8ad2ae4efbf1ae161d4973e343fd945
NeonCardinal <nvnnaidenov@gmail.com>
2022-12-19 10:15:13 +0200

    add

---
+++ 016.PBOnlineExamAprilTwo/001.OscarsCeremony/OscarsCeremony.cs
@@ -0,0 +1,19 @@
+﻿//Chapter PB online exam April 2019, Task 001
+//16.12.2022, 20:25
+using System;
+
+public class OscarsCeremony
+{
+    static void Main()
+    {
+        int rent = int.Parse(Console.ReadLine());
+
+        double statuettesPrice = rent * 0.70;
+        double cateringPrice = statuettesPrice * 0.85;
+        double soundPrice = cateringPrice / 2;
+
+        double total = rent + statuettesPrice + cateringPrice + soundPrice;
+
+        Console.WriteLine($"{total:F2}");
+    }
+}
